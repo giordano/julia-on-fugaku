357bb708c5e7cbf3d760964d8bada0ee3e0ee11e
Mosè Giordano <mose@gnu.org>
2022-04-30 17:01:22 +0100

    Add `BandwidthBenchmark.jl` benchmark

---
+++ benchmarks/bandwidthbenchmarkjl/bench.jl
@@ -0,0 +1,15 @@
+using BandwidthBenchmark
+using DataFrames
+using CSV
+
+CSV.write(joinpath(@__DIR__, "bwbench.csv"), bwbench(; verbose=true))
+let
+    m = bwscaling()
+    df = DataFrame("# Threads" => m[:, 1], "SDaxpy Bandwidth (MB/s)" => m[:, 2])
+    CSV.write(joinpath(@__DIR__, "bwscaling.csv"), df)
+end
+let
+    m = flopsscaling()
+    df = DataFrame("# Threads" => m[:, 1], "Triad Performance (MFlop/s)" => m[:, 2])
+    CSV.write(joinpath(@__DIR__, "flopsscaling.csv"), df)
+end
