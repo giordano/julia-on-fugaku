9d5bf1dec91e5710af5d13aaa1cf5f7891da8546
Mosè Giordano <mose@gnu.org>
2022-05-04 10:44:32 +0100

    Small tweaks to MPI pingpong plot

--- benchmarks/mpi-pingpong/plot.jl
+++ benchmarks/mpi-pingpong/plot.jl
@@ -1,34 +1,43 @@
 using Plots, DelimitedFiles
 
+# Convert decimal Megabyte to binary Gibibyte
+mb_to_gib(x) = x * (1e6 / (2 ^ 30))
+
 function plot_bench()
+    system = "Fugaku"
+    xlims = (1, 2 ^ 23)
+    xticks = (exp2.(0:2:22), ["1 B",   "4 B",   "16 B",   "64 B",   "256 B",
+                              "1 KiB", "4 KiB", "16 KiB", "64 KiB", "256 KiB",
+                              "1 MiB", "4 MiB"])
+
     julia = readdlm(joinpath(@__DIR__, "julia.csv"), ',', Float64; skipstart=1)
     riken = readdlm(joinpath(@__DIR__, "riken.csv"), ',', Float64; skipstart=1)
 
     p = plot(;
-             title = "Communication time of MPI PingPong",
-             xlabel = "message size [bytes]",
+             title = "Latency of MPI PingPong @ $(system)",
+             xlabel = "message size",
              xscale = :log10,
-             xlims = (1, Inf),
-             xticks = floor.(Int, exp10.(0:9)),
+             xlims,
+             xticks,
              ylabel = "time [sec]",
              yscale = :log10,
              legend=:topleft,
              )
-    plot!(p, julia[:, 1], julia[:, 2]; label="MPI.jl", marker=:auto, markersize=3)
-    plot!(p, riken[:, 1], riken[:, 2]; label="Riken-CCS", marker=:auto, markersize=3)
+    plot!(p, julia[:, 1], julia[:, 2]; label="Julia (MPI.jl)", marker=:auto, markersize=3)
+    plot!(p, riken[:, 1], riken[:, 2]; label="C (Riken-CCS)", marker=:auto, markersize=3)
     savefig(joinpath(@__DIR__, "pingpong-time.pdf"))
 
     p = plot(;
-             title = "Throughput of MPI PingPong",
-             xlabel = "message size [bytes]",
+             title = "Throughput of MPI PingPong @ $(system)",
+             xlabel = "message size",
              xscale = :log10,
-             xlims = (1, Inf),
-             xticks = floor.(Int, exp10.(0:9)),
-             ylabel = "throughput [MB/s]",
+             xlims,
+             xticks,
+             ylabel = "throughput [GiB/s]",
              legend=:topleft,
              )
-    plot!(p, julia[:, 1], julia[:, 3]; label="MPI.jl", marker=:auto, markersize=3)
-    plot!(p, riken[:, 1], riken[:, 3]; label="Riken-CCS", marker=:auto, markersize=3)
+    plot!(p, julia[:, 1], mb_to_gib.(julia[:, 3]); label="Julia (MPI.jl)", marker=:auto, markersize=3)
+    plot!(p, riken[:, 1], mb_to_gib.(riken[:, 3]); label="C (Riken-CCS)", marker=:auto, markersize=3)
     savefig(joinpath(@__DIR__, "pingpong-throughput.pdf"))
 
 end
