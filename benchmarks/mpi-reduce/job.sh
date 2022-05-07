ff6f632d79b6710126ac89a2005f0e9b1016215d
Mosè Giordano <mose@gnu.org>
2022-05-07 02:04:18 +0100

    Add MPI reduce benchmark

---
+++ benchmarks/mpi-reduce/job.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#PJM -L "node=384"                # Number of node
+#PJM -L "rscgrp=small"            # Specify resource group
+#PJM -L "elapse=40:00"            # Job run time limit value
+#PJM --mpi "max-proc-per-node=4"  # Upper limit of number of MPI process created at 1 node
+#PJM -S                           # Direction of statistic information file output
+
+# Directory for log of `llio_transfer` and its wrapper `dir_transfer`
+LOGDIR="${TMPDIR}/log"
+
+# Create the log directory if necessary
+mkdir -p "${LOGDIR}"
+
+# Get directory where Julia is placed
+JL_BUNDLE="$(dirname $(julia --startup-file=no -O0 --compile=min -e 'print(Sys.BINDIR)'))"
+
+# Move Julia installation to fast LLIO directory
+/home/system/tool/dir_transfer -l "${LOGDIR}" "${JL_BUNDLE}"
+
+# Do not write empty stdout/stderr files for MPI processes.
+export PLE_MPI_STD_EMPTYFILE=off
+
+mpiexecjl --project=. -np 1536 julia reduce.jl
+
+# Remove Julia installation directory from the cache.
+/home/system/tool/dir_transfer -p -l "${LOGDIR}" "${JL_BUNDLE}"
---
+++ benchmarks/mpi-reduce/plot.jl
@@ -0,0 +1,29 @@
+using Plots, DelimitedFiles
+
+function plot_bench()
+    system = "Fugaku"
+    xlims = (1, 2 ^ 23)
+    xticks = (exp2.(0:2:22), ["1 B",   "4 B",   "16 B",   "64 B",   "256 B",
+                              "1 KiB", "4 KiB", "16 KiB", "64 KiB", "256 KiB",
+                              "1 MiB", "4 MiB"])
+
+    julia = readdlm(joinpath(@__DIR__, "julia.csv"), ',', Float64; skipstart=1)
+    riken = readdlm(joinpath(@__DIR__, "riken.csv"), ',', Float64; skipstart=1)
+
+    p = plot(;
+             title = "Latency of MPI Reduce @ $(system) (384 nodes, 1536 ranks)",
+             titlefont=font(12),
+             xlabel = "message size",
+             xscale = :log10,
+             xlims,
+             xticks,
+             ylabel = "time [sec]",
+             yscale = :log10,
+             legend=:topleft,
+             )
+    plot!(p, julia[:, 1], julia[:, 2]; label="Julia (MPI.jl)", marker=:auto, markersize=3)
+    plot!(p, riken[:, 1], riken[:, 2]; label="C (Riken-CCS)", marker=:auto, markersize=3)
+    savefig(joinpath(@__DIR__, "reduce.pdf"))
+end
+
+plot_bench()
