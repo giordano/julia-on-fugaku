b2c6f86b96dbc3765120d277a9d226183a9d1dc9
Mosè Giordano <mose@gnu.org>
2022-05-14 13:39:15 +0100

    Try newer API of `MPIBenchmarks.jl`

--- benchmarks/mpi-point-to-point/point-to-point-benchmarks.jl
+++ benchmarks/mpi-point-to-point/point-to-point-benchmarks.jl
@@ -1,6 +1,6 @@
 using MPIBenchmarks
 
 # Point-to-point benchmarks
-run(IMBPingPong())
-run(IMBPingPing())
-run(OSULatency())
+benchmark(IMBPingPong())
+benchmark(IMBPingPing())
+benchmark(OSULatency())
