faffa7579e6a048c8ac624573f02b3c5510b7de3
Mosè Giordano <mose@gnu.org>
2022-05-17 01:18:36 +0100

    Add results for Allreduce

--- benchmarks/mpi-collective/plot.jl
+++ benchmarks/mpi-collective/plot.jl
@@ -1,17 +1,17 @@
 using Plots, DelimitedFiles
 
-function plot_bench()
+function plot_bench(name::String)
     system = "Fugaku"
     xlims = (1, 2 ^ 23)
     xticks = (exp2.(0:2:22), ["1 B",   "4 B",   "16 B",   "64 B",   "256 B",
                               "1 KiB", "4 KiB", "16 KiB", "64 KiB", "256 KiB",
                               "1 MiB", "4 MiB"])
 
-    julia = readdlm(joinpath(@__DIR__, "julia.csv"), ',', Float64; skipstart=1)
-    riken = readdlm(joinpath(@__DIR__, "riken.csv"), ',', Float64; skipstart=1)
+    julia = readdlm(joinpath(@__DIR__, "julia_imb_$(lowercase(name)).csv"), ',', Float64; skipstart=1)
+    riken = readdlm(joinpath(@__DIR__, "riken_imb_$(lowercase(name)).csv"), ',', Float64; skipstart=1)
 
     p = plot(;
-             title = "Latency of MPI Allreduce @ $(system) (384 nodes, 1536 ranks)",
+             title = "Latency of MPI $(name) @ $(system) (384 nodes, 1536 ranks)",
              titlefont=font(12),
              xlabel = "message size",
              xscale = :log10,
@@ -21,9 +21,11 @@ function plot_bench()
              yscale = :log10,
              legend=:topleft,
              )
-    plot!(p, julia[:, 1], julia[:, 2]; label="Julia (MPI.jl)", marker=:auto, markersize=3)
+    # For MPI.jl plot average time.  I don't know what time is used in Riken results
+    plot!(p, julia[:, 1], julia[:, 5]; label="Julia (MPI.jl)", marker=:auto, markersize=3)
     plot!(p, riken[:, 1], riken[:, 2]; label="C (Riken-CCS)", marker=:auto, markersize=3)
-    savefig(joinpath(@__DIR__, "allreduce.pdf"))
+    savefig(joinpath(@__DIR__, "$(lowercase(name))-latency.pdf"))
+
 end
 
-plot_bench()
+plot_bench("Allreduce")
