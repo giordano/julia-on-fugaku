c416bb9ce6e87139c735dfdfad9c8bb6b11419c2
Mosè Giordano <mose@gnu.org>
2022-07-22 01:34:18 +0100

    Rerun `axpy` benchmark with single-threaded ARMPL
    Performance is still not great, but much better than the multi-threaded
    library (`_mp`) used with a single thread.

--- benchmarks/blas-axpy/armpl-axpy.jl
+++ benchmarks/blas-axpy/armpl-axpy.jl
@@ -5,7 +5,7 @@ import Libdl: find_library
 
 if "LD_LIBRARY_PATH" in keys(ENV)
     library_path = split(ENV["LD_LIBRARY_PATH"], ':')
-    armpl_path = find_library("libarmpl_ilp64_mp.so", library_path)
+    armpl_path = find_library("libarmpl_ilp64.so", library_path)
     @show BLAS.lbt_forward(armpl_path; clear=true)
 else
     error("Environment variable `LD_LIBRARY_PATH` not set. Did you load the `armpl` module?")
@@ -14,7 +14,7 @@ end
 # Make sure we're using Arm Performance Library
 let
     blases = BLAS.get_config().loaded_libs
-    armpl = findfirst(x -> contains(x.libname, r"libarmpl_ilp64_mp.so$"), blases)
+    armpl = findfirst(x -> contains(x.libname, r"libarmpl_ilp64.so$"), blases)
     @assert !isnothing(armpl)
     @assert blases[armpl].interface === :ilp64
 end
