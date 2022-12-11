5d86cdfc8c7bb2c51515cd2babc0e2561e47a00c
Mosè Giordano <mose@gnu.org>
2022-12-11 23:29:07 +0000

    New run of bwscaling on Ookami with all kernels

--- benchmarks/bandwidthbenchmarkjl/bench.jl
+++ benchmarks/bandwidthbenchmarkjl/bench.jl
@@ -8,7 +8,15 @@ pinthreads(:scatter; places=:numa)
 CSV.write(joinpath(@__DIR__, "bwbench.csv"), bwbench(; verbose=true))
 let
     m = bwscaling()
-    df = DataFrame("# Threads" => m[:, 1], "SDaxpy Bandwidth (MB/s)" => m[:, 2])
+    df = DataFrame("# Threads" => m[:, 1],
+                   "Init Bandwidth (MB/s)" => m[:, 2],
+                   "Copy Bandwidth (MB/s)" => m[:, 3],
+                   "Update Bandwidth (MB/s)" => m[:, 4],
+                   "Triad Bandwidth (MB/s)" => m[:, 5],
+                   "Daxpy Bandwidth (MB/s)" => m[:, 6],
+                   "STriad Bandwidth (MB/s)" => m[:, 7],
+                   "SDaxpy Bandwidth (MB/s)" => m[:, 8],
+                   )
     CSV.write(joinpath(@__DIR__, "bwscaling.csv"), df)
 end
 let
--- benchmarks/bandwidthbenchmarkjl/plot.jl
+++ benchmarks/bandwidthbenchmarkjl/plot.jl
@@ -6,8 +6,8 @@ bwscaling = CSV.read(joinpath(@__DIR__, "bwscaling.csv"), DataFrame)
 flopsscaling = CSV.read(joinpath(@__DIR__, "flopsscaling.csv"), DataFrame)
 latencies = CSV.read(joinpath(@__DIR__, "latencies.csv"), DataFrame)
 
-function plot_scaling(df, title, ylabel)
-    plot(df[:, 1], df[:, 2];
+function plot_scaling(df, title, ylabel, column)
+    plot(df[:, 1], df[:, column] ./ 1000;
          title,
          xlabel="# Threads",
          xticks=0:4:48,
@@ -15,13 +15,14 @@ function plot_scaling(df, title, ylabel)
          marker=:circle,
          markersize=3,
          label="",
-         size=(800, 600),
          )
 end
 
-plot_scaling(bwscaling, "Memory Bandwidth Scaling", "SDaxpy Bandwidth (MB/s)")
-savefig(joinpath(@__DIR__, "bwscaling.pdf"))
-plot_scaling(flopsscaling, "FLOPS Scaling", "Triad Performance (MFlop/s)")
+for (idx, name) in enumerate(("Init", "Copy", "Update", "Triad", "Daxpy", "STriad", "SDaxpy"))
+    plot_scaling(bwscaling, "Memory Bandwidth Scaling for $(name)", "Bandwidth (GB/s)", idx+1)
+    savefig(joinpath(@__DIR__, "bwscaling-$(lowercase(name)).pdf"))
+end
+plot_scaling(flopsscaling, "FLOPS Scaling", "Triad Performance (GFlop/s)", 2)
 savefig(joinpath(@__DIR__, "flopsscaling.pdf"))
 heatmap(Matrix(latencies); c=:viridis, frame=:box)
 savefig(joinpath(@__DIR__, "latencies.pdf"))
